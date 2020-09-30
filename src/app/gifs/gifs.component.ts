4ca2fa5bc7b2287c5a6cc08911d8b28b801ae5c1
SharonFaith <sharonfaith15@gmail.com>
2020-09-30 19:51:26 +0300

    move html file to correct location

--- src/app/gifs/gifs.component.ts
+++ src/app/gifs/gifs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
 
 @Component({
   selector: 'app-gifs',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GifsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
   }
 
 }
