263ca60e96adb8f364eeb4a29c52c5f0d4bb8416
SharonFaith <sharonfaith15@gmail.com>
2020-09-30 20:54:15 +0300

    add first ts format without services of displaying trending gifs

--- src/app/gifs/gifs.component.ts
+++ src/app/gifs/gifs.component.ts
@@ -8,11 +8,32 @@ import { HttpClient } from '@angular/common/http'
   styleUrls: ['./gifs.component.css']
 })
 export class GifsComponent implements OnInit {
+  gifs;
 
   constructor(private http: HttpClient) { }
 
+  gifImages(response) {
+    let imageArray = []
+    response.forEach(element => {
+      imageArray.push({
+        title: element.title,
+        image: element.images.original.url,
+      })
+    });
+
+    return imageArray
+  }
+
+  gifContent() {
+    this.http.get("https://api.giphy.com/v1/gifs/trending?api_key=PWVU2kUYV18ckDvJrsUFWf27iPELR1Lw&limit=25&rating=g").subscribe(response =>{
+      const gifArray = response['data'] && this.gifImages(response['data']);
+      this.gifs = gifArray
+    })
+  }
+
   ngOnInit() {
-    
+    this.gifContent()
+
   }
 
 }
